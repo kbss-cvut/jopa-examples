090cd7e88742284d55dcc737cb9a2483f8e01b36
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2019-11-04 17:52:00 +0100

    [Fix] Fix support for both JSON and JSON-LD message converters in the jsonld demo.

--- jsonld/src/main/java/cz/cvut/kbss/jopa/jsonld/config/RestConfig.java
+++ jsonld/src/main/java/cz/cvut/kbss/jopa/jsonld/config/RestConfig.java
@@ -1,16 +1,14 @@
 /**
  * Copyright (C) 2019 Czech Technical University in Prague
- *
- * This program is free software: you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any
- * later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
- * details. You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * <p>
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * <p>
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 package cz.cvut.kbss.jopa.jsonld.config;
 
@@ -22,30 +20,34 @@ import cz.cvut.kbss.jsonld.JsonLd;
 import cz.cvut.kbss.jsonld.jackson.JsonLdModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Collections;
 import java.util.List;
 
 @Configuration
-public class RestConfig extends WebMvcConfigurerAdapter {
+public class RestConfig extends WebMvcConfigurationSupport {
 
-    @Bean
+    @Bean(name = "jsonLdMapper")
     public ObjectMapper jsonLdObjectMapper() {
         final ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         // Here we register the JSON-LD serialization/deserialization module
         final JsonLdModule module = new JsonLdModule();
-        module.configure(ConfigParam.SCAN_PACKAGE, "cz.cvut.kbss.jopa.jsonld");
+        // Package scan is important for polymorphic deserialization
+        module.configure(ConfigParam.SCAN_PACKAGE, "cz.cvut.kbss.jopa.example08");
         objectMapper.registerModule(module);
         return objectMapper;
     }
 
-    @Bean
+    @Primary
+    @Bean(name = "objectMapper")
     public ObjectMapper objectMapper() {
         final ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
@@ -56,11 +58,7 @@ public class RestConfig extends WebMvcConfigurerAdapter {
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         converters.add(createJsonLdMessageConverter());
-        converters.add(createDefaultMessageConverter());
-    }
-
-    private HttpMessageConverter<?> createDefaultMessageConverter() {
-        return new MappingJackson2HttpMessageConverter(objectMapper());
+        super.addDefaultHttpMessageConverters(converters);
     }
 
     private HttpMessageConverter<?> createJsonLdMessageConverter() {
