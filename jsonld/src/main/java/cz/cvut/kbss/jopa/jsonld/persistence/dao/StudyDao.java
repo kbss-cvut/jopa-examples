c4a95bb326ab628462259044c28e401e8d550812
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2023-01-31 15:43:22 +0100

    [Fix] Fix an issue with persisting organizations in jsonld demo.
    The issue is caused by DtoMapper creating multiple instances representing the same organization when referenced by different user instances.

--- jsonld/src/main/java/cz/cvut/kbss/jopa/jsonld/persistence/dao/StudyDao.java
+++ jsonld/src/main/java/cz/cvut/kbss/jopa/jsonld/persistence/dao/StudyDao.java
@@ -1,16 +1,14 @@
 /**
  * Copyright (C) 2019 Czech Technical University in Prague
- *
- * This program is free software: you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any
- * later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
- * details. You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * <p>
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * <p>
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 package cz.cvut.kbss.jopa.jsonld.persistence.dao;
 
@@ -35,22 +33,27 @@ public class StudyDao extends BaseDao<Study> {
     @Override
     public void persist(Study entity) {
         persistUsersIfNecessary(entity.getAdministrators());
+        // Flush changes to prevent issues with repeated persist attempts for different instances of the same
+        // Organization (or User) created by DTO mapper
+        em.flush();
         persistUsersIfNecessary(entity.getParticipants());
+        em.flush();
         assert entity.getAuthor() != null;
         super.persist(entity);
     }
 
     private void persistUsersIfNecessary(Collection<User> users) {
         if (users != null && !users.isEmpty()) {
-            users.stream().filter(admin -> !userDao.exists(admin.getUri()))
-                 .forEach(userDao::persist);
+            users.stream().filter(user -> !userDao.exists(user.getUri())).forEach(userDao::persist);
         }
     }
 
     @Override
     public void update(Study entity) {
         persistUsersIfNecessary(entity.getAdministrators());
+        em.flush();
         persistUsersIfNecessary(entity.getParticipants());
+        em.flush();
         super.update(entity);
     }
 }
